ee25e51dafb4a1f844f65e4afd778e0fd528d579
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-07-20 19:02:59 +0530

    .added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
         <Link to="/">
           <img src={logo} alt="logo" className='w-24 object-contain'/>
         </Link>
-        <Link to="/create-post" className='font-inter font-medium bg-[gray] text-white px-4 py-2 rounded-md'>Imag!ne</Link>
+        <Link to="/create-post" className='font-inter font-medium bg-[gray] text-white px-4 py-2 rounded-md'>Imag!ne.</Link>
       </header>
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[clac(100vh-73px)]'>
         <Routes>
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -33,7 +33,7 @@ const CreatePost = () => {
           prompt: form.prompt,
           photo: selectedImage,
         };
-        const response = await fetch("http://localhost:8080/api/v1/post",{
+        const response = await fetch("https://imagine-1swi.onrender.com/api/v1/post",{
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -58,7 +58,7 @@ const CreatePost = () => {
     if(form.prompt){
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle',{
+        const response = await fetch('https://imagine-1swi.onrender.com/api/v1/dalle',{
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
